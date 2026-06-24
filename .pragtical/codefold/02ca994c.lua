b9ac73318eb7d08e9a68dd6c2d0f1bc4c3c286ca
JimHopper150716 <kingarthurofgallifrey@gmail.com>
2026-06-24 03:36:19 +0200

    Auto-saved HTML update

--- .pragtical/codefold/02ca994c.lua
+++ .pragtical/codefold/02ca994c.lua
@@ -5,6 +5,18 @@ return {
       ["indent"] = 4,
       ["text"] = "    :root {\
 "
+    },
+    [2] = {
+      ["line"] = 673,
+      ["indent"] = 0,
+      ["text"] = "<details>\
+"
+    },
+    [3] = {
+      ["line"] = 670,
+      ["indent"] = 4,
+      ["text"] = "    <div class=\"prose\">\
+"
     }
   },
   ["path"] = "index.html"
